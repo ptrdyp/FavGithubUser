9c8e5fb297b5058d1f666a6b39277d8e6c1db16d
ptrdyp <alviany.prameswari@gmail.com>
2023-09-29 09:11:26 +0700

    add splash activity

---
+++ app/src/main/java/com/dicoding/favgithubuser/ui/splash/SplashActivity.kt
@@ -0,0 +1,61 @@
+package com.dicoding.favgithubuser.ui.splash
+
+import android.content.Intent
+import android.content.res.ColorStateList
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import com.dicoding.favgithubuser.R
+import com.dicoding.favgithubuser.databinding.ActivitySplashBinding
+import com.dicoding.favgithubuser.ui.main.MainActivity
+import com.dicoding.favgithubuser.ui.setting.SettingPreferences
+import com.dicoding.favgithubuser.ui.setting.dataStore
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    private val splashTimeOut:Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashTimeOut)
+
+        val settingPreferences = SettingPreferences.getInstance(dataStore)
+
+        lifecycleScope.launch {
+            settingPreferences.getThemeSetting().collect{ isDarkModeActive ->
+                if (isDarkModeActive){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    binding.imageView.changeIconColor(R.color.on_secondary)
+                    binding.tvSplashText.changeTextColor(R.color.on_secondary)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    binding.imageView.changeIconColor(R.color.dark_on_secondary)
+                    binding.tvSplashText.changeTextColor(R.color.dark_on_secondary)
+
+                }
+            }
+        }
+    }
+
+    fun ImageView.changeIconColor(@ColorRes color: Int){
+        imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this.context, color))
+    }
+
+    fun TextView.changeTextColor(@ColorRes color: Int){
+        setTextColor(ContextCompat.getColor(this.context, color))
+    }
+}
